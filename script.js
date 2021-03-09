1668a3621ffc2055a408451f85f623e4fa20268b
Abdulqadir90 <abdiqaadirabdillahi@gmail.com>
2021-03-09 01:21:36 +0000

    lv 100 and 350 done

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
-//You can edit ALL of the code here
-function setup() {
-  const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
+let showsArr = [];
+
+function getShows() {
+  fetch(`https://api.tvmaze.com/shows/82/episodes`)
+    .then(res => res.json())
+    .then(data => {
+      data.map(obj => {
+        let objData = {name: obj.name,season:  obj.season, number: obj.number, image: obj.image.medium,summary: obj.summary}
+        showsArr.push(objData);
+      });
+      showsArr.length === 73 ? displayShows(): null;
+    })
 }
 
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+function episodeCode(data, option) {
+  if (data[option] < 10) {
+    return `0${data[option]}`;
+  } else {
+    return `${data[option]}`;
+  }
 }
 
-window.onload = setup;
+function sortNumber(data) {
+  return `S${episodeCode(data, 'season')}E${episodeCode(data, 'number')}`
+}
+
+function displayShows() {
+  document.querySelector('.display').innerHTML = showsArr.map(obj => {
+    return `
+      <div class="show">
+        <div class="title">${obj.name} - ${sortNumber(obj)}</div>
+        <img class="image" src="${obj.image}">
+        <div class="summary">${obj.summary}</div>
+      </div>
+    `
+  }).join(''); 
+}
+
+getShows();
+
