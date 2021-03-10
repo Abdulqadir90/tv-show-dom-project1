6ee6193ca09ddd3ce22feaeaff00b669cff22ea9
Abdulqadir90 <abdiqaadirabdillahi@gmail.com>
2021-03-10 23:00:26 +0000

    lv 200 - 300 done

--- script.js
+++ script.js
@@ -1,40 +1,83 @@
 let showsArr = [];
+let selectOption = [];
+const search = document.querySelector('.search');
+const select = document.querySelector('.options');
 
-function getShows() {
+const getShows = () => {
   fetch(`https://api.tvmaze.com/shows/82/episodes`)
-    .then(res => res.json())
-    .then(data => {
-      data.map(obj => {
-        let objData = {name: obj.name,season:  obj.season, number: obj.number, image: obj.image.medium,summary: obj.summary}
+    .then((res) => res.json())
+    .then((data) => {
+      data.map((obj) => {
+        let objData = {
+          name: obj.name,
+          season: obj.season,
+          number: obj.number,
+          image: obj.image.medium,
+          summary: obj.summary,
+        };
         showsArr.push(objData);
       });
-      showsArr.length === 73 ? displayShows(): null;
+      showsArr.length === 73 ? displayShows() : null;
+    });
+}
+
+
+const episodeCode = (data, option) => data[option] < 10 ? `0${data[option]}`: `${data[option]}`;
+ 
+const sortNumber = (data) => `S${episodeCode(data, "season")}E${episodeCode(data, "number")}`;
+
+const displayShows = () => {
+  document.querySelector(".display").innerHTML = showsArr
+    .map((obj) => {
+      options(obj);
+     return createShowCards(obj);
     })
+    .join("");
 }
 
-function episodeCode(data, option) {
-  if (data[option] < 10) {
-    return `0${data[option]}`;
-  } else {
-    return `${data[option]}`;
+const options = (data) => {
+  let optionSelected = document.createElement('option');
+  optionSelected.value = `${sortNumber(data)} - ${data.name}`;
+  optionSelected.innerHTML = `${sortNumber(data)} - ${data.name}`;
+  select.appendChild(optionSelected);
+}
+
+const createShowCards = (data) => {
+  return `
+    <div class="show">
+      <div class="title">${data.name} - ${sortNumber(data)}</div>
+      <img class="image" src="${data.image}">
+      <div class="summary">${data.summary}</div>
+    </div>  
+  `
+}
+
+search.addEventListener('input', (e) => {
+  if (showsArr.length > 0) {
+    let displaying = showsArr.filter((obj) => {
+      if (obj.name.toLowerCase().includes(e.target.value.toLowerCase()) || obj.summary.toLowerCase().includes(e.target.value.toLowerCase())) {
+        return obj;
+      }
+    });
+    if (e.target.value.length > 0) {
+      document.querySelector('.displayCount').innerHTML = `Displaying ${displaying.length}/${showsArr.length} episodes`;
+    } else {
+      document.querySelector('.displayCount').innerHTML = '';
+    }
+    document.querySelector(".display").innerHTML = displaying.map(obj => createShowCards(obj))
+    .join("");
   }
-}
+});
 
-function sortNumber(data) {
-  return `S${episodeCode(data, 'season')}E${episodeCode(data, 'number')}`
-}
-
-function displayShows() {
-  document.querySelector('.display').innerHTML = showsArr.map(obj => {
-    return `
-      <div class="show">
-        <div class="title">${obj.name} - ${sortNumber(obj)}</div>
-        <img class="image" src="${obj.image}">
-        <div class="summary">${obj.summary}</div>
-      </div>
-    `
-  }).join(''); 
-}
+select.addEventListener('change', (e) => {
+  document.querySelector('.display').innerHTML = showsArr.filter((obj) => {
+    if (e.target.value.toLowerCase().includes(obj.name.toLowerCase())) {
+      console.log('got in here');
+      return obj;
+    } else if (e.target.value === 'showAll') {
+      return showsArr;
+    }
+  }).map(obj => createShowCards(obj)).join(" ");
+})
 
 getShows();
-
